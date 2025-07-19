7d1b705abc675120fa2c95aefc94c31505177846
Adrien4193 <adrien.fleury93@gmail.com>
2025-07-19 11:30:04 +0200

    Use C++23.

--- Applications/Application.cpp
+++ Applications/Application.cpp
@@ -1,11 +1,14 @@
-#include <Template/Lib1/Lib1.hpp>
-#include <Template/Lib2/Lib2.hpp>
+#include <print>
 
 #include <fmt/format.h>
+
+#include <Template/Lib1/Lib1.hpp>
+#include <Template/Lib2/Lib2.hpp>
 
 auto main(int argc, const char **argv) -> int
 {
     fmt::println("argc: {}", argc);
+    std::println("This is from std23: {}", argc);
 
     for (auto i = 0; i < argc; ++i)
     {
