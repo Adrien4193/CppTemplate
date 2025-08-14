e6ccc799aed36ba39ba4075aae3f9c6be5f6a381
Adrien4193 <adrien.fleury93@gmail.com>
2025-08-14 18:49:36 +0200

    Remove fmt and keep some clang-tidy checks.

--- Applications/Application.cpp
+++ Applications/Application.cpp
@@ -1,20 +1,18 @@
 #include <print>
 
-#include <fmt/format.h>
-
 #include <Template/Lib1/Lib1.hpp>
 #include <Template/Lib2/Lib2.hpp>
 
+// NOLINTNEXTLINE(bugprone-exception-escape)
 auto main(int argc, const char **argv) -> int
 {
-    fmt::println("argc: {}", argc);
-    std::println("This is from std23: {}", argc);
+    std::println("argc: {}", argc);
 
     for (auto i = 0; i < argc; ++i)
     {
-        fmt::println("argv [{}]: {}", i, argv[i]);
+        std::println("argv [{}]: {}", i, argv[i]);
     }
 
-    fmt::println("Lib1: {}", Template::Lib1(1));
-    fmt::println("Lib2: {}", Template::Lib2(2));
+    std::println("Lib1: {}", Template::Lib1(1));
+    std::println("Lib2: {}", Template::Lib2(2));
 }
--- Source/Template/Lib1/Lib1.cpp
+++ Source/Template/Lib1/Lib1.cpp
@@ -1,13 +1,9 @@
 #include "Lib1.hpp"
 
-#include <fmt/format.h>
-
 namespace Template
 {
     auto Lib1(int value) -> int
     {
-        fmt::println("Lib1 value: {}", value);
-
         return value;
     }
 }
