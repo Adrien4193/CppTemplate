2cfb40bd344a10714551acc197443747f5390920
Adrien4193 <39053578+Adrien4193@users.noreply.github.com>
2025-09-30 20:07:02 +0200

    Add CMake components. (#4)

--- CMake/Package.cmake
+++ CMake/Package.cmake
@@ -1,47 +1,54 @@
-function(configure_package)
-    install(
-        EXPORT ${PROJECT_NAME}Targets
-        FILE ${PROJECT_NAME}Targets.cmake
-        DESTINATION cmake
-        NAMESPACE ${PROJECT_NAME}::
-    )
+set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
+set(CPACK_PACKAGE_DESCRIPTION ${PROJECT_DESCRIPTION})
+set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE.md")
+set(CPACK_RESOURCE_FILE_README "${PROJECT_SOURCE_DIR}/README.md")
 
-    export(
-        EXPORT ${PROJECT_NAME}Targets
-        FILE ${PROJECT_NAME}Targets.cmake
-        NAMESPACE ${PROJECT_NAME}::
-    )
+include(CPack)
 
-    include(CMakePackageConfigHelpers)
+include(CMakePackageConfigHelpers)
 
-    configure_package_config_file(
-        ${PROJECT_SOURCE_DIR}/CMake/PackageConfig.cmake.in
-        ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
-        INSTALL_DESTINATION cmake
-    )
+configure_package_config_file(
+    ${PROJECT_SOURCE_DIR}/CMake/PackageConfig.cmake.in
+    ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+    INSTALL_DESTINATION ${PROJECT_INSTALL_CONFIGDIR}
+)
 
-    write_basic_package_version_file(
-        ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
-        VERSION ${PROJECT_VERSION}
-        COMPATIBILITY SameMajorVersion
-    )
+write_basic_package_version_file(
+    ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
+    VERSION ${PROJECT_VERSION}
+    COMPATIBILITY SameMajorVersion
+)
 
-    install(
-        FILES
-        ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
-        ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
-        DESTINATION cmake
-    )
+install(
+    FILES
+    ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
+    DESTINATION ${PROJECT_INSTALL_CONFIGDIR}
+    COMPONENT Development
+)
 
-    include(InstallRequiredSystemLibraries)
+set(CMAKE_INSTALL_SYSTEM_RUNTIME_COMPONENT SystemRuntime)
 
-    set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
-    set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
-    set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
-    set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
-    set(CPACK_PACKAGE_DESCRIPTION ${PROJECT_DESCRIPTION})
-    set(CPACK_RESOURCE_FILE_LICENSE ${PROJECT_SOURCE_DIR}/LICENSE.md)
-    set(CPACK_RESOURCE_FILE_README ${PROJECT_SOURCE_DIR}/README.md)
+include(InstallRequiredSystemLibraries)
 
-    include(CPack)
-endfunction()
+cpack_add_component(
+    SystemRuntime
+    DISPLAY_NAME "System runtime"
+    DESCRIPTION "System runtime libraries"
+)
+
+cpack_add_component(
+    Runtime
+    DISPLAY_NAME "Runtime"
+    DESCRIPTION "Runtime artifacts"
+)
+
+cpack_add_component(
+    Development
+    DISPLAY_NAME "Development"
+    DESCRIPTION "Development artifacts"
+    DEPENDS Runtime
+)
